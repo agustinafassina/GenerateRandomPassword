f375feef870b2260e5d849ff477340fb96a011c7
Agustina Fassina | ENG <agustinafassina@gmail.com>
2025-08-05 08:34:09 -0300

    add program c#

---
+++ csharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class PasswordGenerator
+{
+    static void Main()
+    {
+        int length = 16;
+        string password = GenerateSecurePassword(length);
+        Console.WriteLine($"Generated Password with net9: {password}");
+    }
+
+    static string GenerateSecurePassword(int length)
+    {
+        const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=[]{}|;:,.<>?";
+        RNGCryptoServiceProvider rnd = new RNGCryptoServiceProvider();
+
+        char[] passwordChars = new char[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            byte[] randomByte = new byte[1];
+            rnd.GetBytes(randomByte);
+            int idx = randomByte[0] % validChars.Length;
+            passwordChars[i] = validChars[idx];
+        }
+
+        return new string(passwordChars);
+    }
+}
