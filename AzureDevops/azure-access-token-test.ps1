0ec9d6c70210c0b3897551a1983da0e015486d49
Agustina Fassina | ENG <agustinafassina@gmail.com>
2025-10-11 13:56:40 -0300

    organizationTest

--- AzureDevops/azure-access-token-test.ps1
+++ AzureDevops/azure-access-token-test.ps1
@@ -4,4 +4,4 @@ $headers = @{
     Authorization = "Basic " + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PersonalAccessToken)"))
 }
 
-Invoke-RestMethod -Uri "https://dev.azure.com/innovation0294/_apis/distributedtask/pools?api-version=6.0" -Headers $headers
+Invoke-RestMethod -Uri "https://dev.azure.com/organizationTest/_apis/distributedtask/pools?api-version=6.0" -Headers $headers
