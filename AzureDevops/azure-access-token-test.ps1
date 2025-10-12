5fe83dfcd515980e6aa073a0a8547409be284470
Agustina Fassina | ENG <agustinafassina@gmail.com>
2025-10-12 15:25:27 -0300

    script move to other repository

--- AzureDevops/azure-access-token-test.ps1
+++
@@ -1,7 +0,0 @@
-$PersonalAccessToken = ""
-
-$headers = @{
-    Authorization = "Basic " + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PersonalAccessToken)"))
-}
-
-Invoke-RestMethod -Uri "https://dev.azure.com/organizationTest/_apis/distributedtask/pools?api-version=6.0" -Headers $headers
